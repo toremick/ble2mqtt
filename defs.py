fafefcd7c1314a14068af3cdf75edcbc92246ed8
tore <tore@eposten.net>
2020-11-02 13:57:30 +0000

    added support for more sensors

--- defs.py
+++ defs.py
@@ -1,53 +1,161 @@
-battery_cfg="""
-{"unit_of_measurement":"%",
-"device_class":"battery",
-"value_template":"{{ value_json.battery }}",
-"state_topic":"ble2mqtt/MACADDRESS",
-"json_attributes_topic":"ble2mqtt/MACADDRESS",
-"name":"MACADDRESS_battery",
-"unique_id":"MACADDRESS_battery_ble2mqtt",
-"device":{"identifiers":["ble2mqtt_MACADDRESS"],
-"name":"MACADDRESS",
-"sw_version":"ble2mqtt 1.0",
-"model":"iBS03T",
-"manufacturer":"IGNICS"},
-"availability_topic":"ble2mqtt/state"}"""
+#battery_cfg="""
+#{"unit_of_measurement":"%",
+#"device_class":"battery",
+#"value_template":"{{ value_json.battery }}",
+#"state_topic":"ble2mqtt/MACADDRESS",
+#"json_attributes_topic":"ble2mqtt/MACADDRESS",
+#"name":"MACADDRESS_battery",
+#"unique_id":"MACADDRESS_battery_ble2mqtt",
+#"device":{"identifiers":["ble2mqtt_MACADDRESS"],
+#"name":"MACADDRESS",
+#"sw_version":"ble2mqtt 1.0",
+#"model":"MODELNAME",
+#"manufacturer":"MANUFACTURENAME"},
+#"availability_topic":"ble2mqtt/state"}"""
+
+battery_cfg = """
+{
+  "availability_topic": "ble2mqtt/state",
+  "device": {
+    "identifiers": [
+      "ble2mqtt_MACADDRESS"
+    ],
+    "manufacturer": "MANUFACTURENAME",
+    "model": "MODELNAME",
+    "name": "MACADDRESS",
+    "sw_version": "ble2mqtt 1.0"
+  },
+  "device_class": "battery",
+  "json_attributes_topic": "ble2mqtt/MACADDRESS",
+  "name": "MACADDRESS_battery",
+  "state_topic": "ble2mqtt/MACADDRESS",
+  "unique_id": "MACADDRESS_battery_ble2mqtt",
+  "unit_of_measurement": "%",
+  "value_template": "{{ value_json.battery }}"
+}"""
+
+#signal_cfg = """
+#{"icon":"mdi:signal",
+#"unit_of_measurement":"lqi",
+#"value_template":"{{ value_json.linkquality }}",
+#"state_topic":"ble2mqtt/MACADDRESS",
+#"json_attributes_topic":"ble2mqtt/MACADDRESS",
+#"name":"MACADDRESS_linkquality",
+#"unique_id":"MACADDRESS_linkquality_ble2mqtt",
+#"device":{"identifiers":["ble2mqtt_MACADDRESS"],
+#"name":"MACADDRESS",
+#"sw_version":"ble2mqtt 1.0",
+#"model":"MODELNAME",
+#"manufacturer":"MANUFACTURENAME"},
+#"availability_topic":"ble2mqtt/state"}"""
 
 signal_cfg = """
-{"icon":"mdi:signal",
-"unit_of_measurement":"lqi",
-"value_template":"{{ value_json.linkquality }}",
-"state_topic":"ble2mqtt/MACADDRESS",
-"json_attributes_topic":"ble2mqtt/MACADDRESS",
-"name":"MACADDRESS_linkquality",
-"unique_id":"MACADDRESS_linkquality_ble2mqtt",
-"device":{"identifiers":["ble2mqtt_MACADDRESS"],
-"name":"MACADDRESS",
-"sw_version":"ble2mqtt 1.0",
-"model":"iBS03T",
-"manufacturer":"IGNICS"},
-"availability_topic":"ble2mqtt/state"}"""
+{
+  "availability_topic": "ble2mqtt/state",
+  "device": {
+    "identifiers": [
+      "ble2mqtt_MACADDRESS"
+    ],
+    "manufacturer": "MANUFACTURENAME",
+    "model": "MODELNAME",
+    "name": "MACADDRESS",
+    "sw_version": "ble2mqtt 1.0"
+  },
+  "icon": "mdi:signal",
+  "json_attributes_topic": "ble2mqtt/MACADDRESS",
+  "name": "MACADDRESS_signal",
+  "state_topic": "ble2mqtt/MACADDRESS",
+  "unique_id": "MACADDRESS_linkquality_ble2mqtt",
+  "unit_of_measurement": "lqi",
+  "value_template": "{{ value_json.linkquality }}"
+}"""
+
+#temperature_cfg = """
+#{"unit_of_measurement":"°C",
+#"device_class":"temperature",
+#"value_template":"{{ value_json.temperature }}",
+#"state_topic":"ble2mqtt/MACADDRESS",
+#"json_attributes_topic":"ble2mqtt/MACADDRESS",
+#"name":"MACADDRESS_temperature",
+#"unique_id":"MACADDRESS_temperature_ble2mqtt",
+#"device":{"identifiers":["ble2mqtt_MACADDRESS"],
+#"name":"MACADDRESS",
+#"sw_version":"ble2mqtt 1.0",
+#"model":"MODELNAME",
+#"manufacturer":"MANUFACTURENAME"},
+#"availability_topic":"ble2mqtt/state"}"""
 
 temperature_cfg = """
-{"unit_of_measurement":"°C",
-"device_class":"temperature",
-"value_template":"{{ value_json.temperature }}",
-"state_topic":"ble2mqtt/MACADDRESS",
-"json_attributes_topic":"ble2mqtt/MACADDRESS",
-"name":"MACADDRESS_temperature",
-"unique_id":"MACADDRESS_temperature_ble2mqtt",
-"device":{"identifiers":["ble2mqtt_MACADDRESS"],
-"name":"MACADDRESS",
-"sw_version":"ble2mqtt 1.0",
-"model":"iBS03T",
-"manufacturer":"IGNICS"},
-"availability_topic":"ble2mqtt/state"}"""
+{
+  "availability_topic": "ble2mqtt/state",
+  "device": {
+    "identifiers": [
+      "ble2mqtt_MACADDRESS"
+    ],
+    "manufacturer": "MANUFACTURENAME",
+    "model": "MODELNAME",
+    "name": "MACADDRESS",
+    "sw_version": "ble2mqtt 1.0"
+  },
+  "device_class": "temperature",
+  "json_attributes_topic": "ble2mqtt/MACADDRESS",
+  "name": "MACADDRESS_temperature",
+  "state_topic": "ble2mqtt/MACADDRESS",
+  "unique_id": "MACADDRESS_temperature_ble2mqtt",
+  "unit_of_measurement": "°C",
+  "value_template": "{{ value_json.temperature }}"
+}"""
 
+#humidity_cfg = """
+#{"unit_of_measurement":"%",
+#"device_class":"humidity",
+#"value_template":"{{ value_json.humidity }}",
+#"state_topic":"ble2mqtt/MACADDRESS",
+#"json_attributes_topic":"ble2mqtt/MACADDRESS",
+#"name":"MACADDRESS_humidity",
+#"unique_id":"MACADDRESS_humidity_ble2mqtt",
+#"device":{"identifiers":["ble2mqtt_MACADDRESS"],
+#"name":"MACADDRESS",
+#"sw_version":"ble2mqtt 1.0",
+#"model":"MODELNAME",
+#"manufacturer":"MANUFACTURENAME"},
+#"availability_topic":"ble2mqtt/state"}"""
+
+humidity_cfg = """
+{
+  "availability_topic": "ble2mqtt/state",
+  "device": {
+    "identifiers": [
+      "ble2mqtt_MACADDRESS"
+    ],
+    "manufacturer": "MANUFACTURENAME",
+    "model": "MODELNAME",
+    "name": "MACADDRESS",
+    "sw_version": "ble2mqtt 1.0"
+  },
+  "device_class": "humidity",
+  "json_attributes_topic": "ble2mqtt/MACADDRESS",
+  "name": "MACADDRESS_humidity",
+  "state_topic": "ble2mqtt/MACADDRESS",
+  "unique_id": "MACADDRESS_humidity_ble2mqtt",
+  "unit_of_measurement": "%",
+  "value_template": "{{ value_json.humidity }}"
+}"""
 
 topicmsg = """
 {"battery":BATT,
 "linkquality":LINK,
 "temperature":TEMP}"""
+
+
+topicmsgwhumid = """
+{"battery":BATT,
+"linkquality":LINK,
+"temperature":TEMP,
+"humidity":HUMID}"""
+
+
+
 
 
 click_cfg = """
@@ -60,6 +168,6 @@ click_cfg = """
 "device":{"identifiers":["ble2mqtt_MACADDRESS"],
 "name":"MACADDRESS",
 "sw_version":"ble2mqtt 1.0",
-"model":"iBS03T",
-"manufacturer":"IGNICS"},
+"model":"MODELNAME",
+"manufacturer":"MANUFACTURENAME"},
 "availability_topic":"ble2mqtt/state"}"""
--- main.py
+++ main.py
@@ -21,7 +21,41 @@ def getTemp(msg):
     temp = integer[0]/100.0
     return temp
 
-    
+
+def ruuviTemp(msg):
+    val_one = int(msg[18:20], 16)
+    dec = int(msg[20:22], 16)
+    temp = round(val_one + (dec/100),2)
+    return temp
+
+
+def xiaomiTemp(msg):
+    val_one = int(msg[20:24], 16)
+    temp = val_one/10.0
+    return temp
+
+
+def xiaomiHum(msg):
+    humi = int(msg[24:26], 16)
+    return humi
+
+def xiaomiBatt(msg):
+    humi = int(msg[26:28], 16)
+    return humi
+
+
+def ruuviHum(msg):
+    humi = int(msg[16:18], 16)
+    return humi*0.5
+
+def ruuviPress(msg):
+    val_one = int(msg[22:24], 16)
+    val_two = int(msg[24:26], 16)
+    vals = (val_one, val_two)
+    btarray = bytearray(vals)
+    integer = struct.unpack('h'*(len(btarray)//2), btarray)
+    temp = (integer[0]/10)
+    return temp
 
 def getBatt(msg):
     val_one = int(msg[18:20], 16)
@@ -30,19 +64,39 @@ def getBatt(msg):
     vals = (val_one, val_two)
     btarray = bytearray(vals)
     integer = struct.unpack('h'*(len(btarray)//2), btarray)
-    temp = integer[0]/100.0
+    battval = integer[0]/100
+    temp = int((battval/3.3)*100)
+    
     return temp
+
+
+
+def ruuviBatt(msg):
+    battval =  int(msg[38:42], 16)
+    temp = int(((battval/1000.0)/3.3)*100)
+    if temp >= 100:
+        temp = 100
+    return temp
+
+def check_if_string_in_file(string_to_search):
+    with open("/opt/ble2mqtt/sensors.state", 'r+') as read_obj:
+        for line in read_obj:
+            if string_to_search in line:
+                return False
+        read_obj.write(string_to_search + "\n")
+    return True
 
     
 def on_message(client, userdata, message):
     try:
-        client.publish("ble2mqtt/state", "online")
         data = str(message.payload.decode("utf-8"))
         data = data.split(',')
-        mac = data[1]
+        mac = "BLE" + data[1]
         signal = data[3]
         payload = data[4]
         senstype = payload[0:14]
+        tore = 1
+        humid = 0
         if senstype  == "02010612FF0D00":
             extra = payload[22:24]
             if extra == '01':
@@ -51,56 +105,103 @@ def on_message(client, userdata, message):
                 button = "off"    
             batt = getBatt(payload)
             temp = getTemp(payload)
-            batt = round((float(batt)/3.3)*100, 2)
-            topic = "ignics/" + str(mac) + "/"
-            
-            batt_cfg = battery_cfg.replace('MACADDRESS', str(mac))
-            temp_cfg = temperature_cfg.replace('MACADDRESS', str(mac))
-            link_cfg = signal_cfg.replace('MACADDRESS', str(mac))
-            clicker_cfg = click_cfg.replace('MACADDRESS', str(mac))
-            
-            tpmsg = topicmsg.replace('BATT', str(batt))
-            tpmsg = tpmsg.replace('TEMP', str(temp))
-            tpmsg = tpmsg.replace('LINK', str(signal))
-            
+            model = "iBS03T"
+            manu = "INGICS"
+
+
+        elif senstype == "02010611FF9904":
+            temp= ruuviTemp(payload)
+            humid = ruuviHum(payload)
+            batt = ruuviBatt(payload)
+            model = "ruuvitag"
+            manu = "RUUVI"
+
+        elif senstype == "10161A18A4C138":
+            temp = xiaomiTemp(payload)
+            humid = xiaomiHum(payload)
+            batt = xiaomiBatt(payload)
+            model = "LYWSD003MMC"
+            manu = "XIAOMI"
+
+
+
+
+        topic = "ignics/" + str(mac) + "/"
+        batt_cfg = battery_cfg.replace('MACADDRESS', str(mac))
+        batt_cfg = batt_cfg.replace('MODELNAME', str(model))
+        batt_cfg = batt_cfg.replace('MANUFACTURENAME', str(manu))
+           
+        temp_cfg = temperature_cfg.replace('MACADDRESS', str(mac))
+        temp_cfg = temp_cfg.replace('MODELNAME', str(model))
+        temp_cfg = temp_cfg.replace('MANUFACTURENAME', str(manu))
+        link_cfg = signal_cfg.replace('MACADDRESS', str(mac))
+        link_cfg = link_cfg.replace('MODELNAME', str(model))
+        link_cfg = link_cfg.replace('MANUFACTURENAME', str(manu))
+        clicker_cfg = click_cfg.replace('MACADDRESS', str(mac))
+        clicker_cfg = clicker_cfg.replace('MODELNAME', str(model))
+        clicker_cfg = clicker_cfg.replace('MANUFACTURENAME', str(manu))
+                  
+        if humid > 1:            
+            hum_cfg = humidity_cfg.replace('MACADDRESS', str(mac))
+            hum_cfg = hum_cfg.replace('MODELNAME', str(model))
+            hum_cfg = hum_cfg.replace('MANUFACTURENAME', str(manu))
+
+        
+        
+        
+            tpmsg = topicmsgwhumid.replace('HUMID', str(humid))
+            tpmsg = tpmsg.replace('BATT', str(batt))
+        else:
+            tpmsg = topicmsg.replace('BATT', str(batt))    
+        tpmsg = tpmsg.replace('TEMP', str(temp))
+        tpmsg = tpmsg.replace('LINK', str(signal))
+
+        if check_if_string_in_file(mac):
             client.publish("homeassistant/sensor/" + mac + "/battery/config", batt_cfg, retain=True)
             client.publish("homeassistant/sensor/" + mac + "/temperature/config", temp_cfg, retain=True)
             client.publish("homeassistant/sensor/" + mac + "/linkquality/config", link_cfg, retain=True)
-            client.publish("ble2mqtt/" + mac, tpmsg)
+            if humid > 1:
+                client.publish("homeassistant/sensor/" + mac + "/humidity/config", hum_cfg, retain=True)
+        client.publish("ble2mqtt/" + mac, tpmsg)
+        client.publish("ble2mqtt/state", "online")
                 
     except Exception as e:
-        print("exception: ")
+        print("on message exception: ")
         print(e)
 
 
+def on_disconnect(client, userdata, rc):
+    print("disconnecting reason  "  +str(rc))
+    
+
+def on_connect(client, userdata, flags, rc):
+    if rc==0:
+        print("connected OK")
+    else:
+        print("Bad connection Returned code=",rc)
+
+
 def connectmqtt():
-    client = mqtt.Client("ble2mqttscript")	
+    client = mqtt.Client("ble2mqttscript-test")	
     if broker_username != "":
         client.username_pw_set(broker_username, broker_password)
         print("logging in")
-    client.connect(broker_address)
+    client.on_connect=on_connect
+    client.on_disconnect=on_disconnect
+    client.connect(broker_address, port=1883, keepalive=600)
     client.subscribe("ble2mqtt/sensors/#")
     client.publish("ble2mqtt/state", "online")
     client.on_message=on_message
     client.loop_forever()
 
 
-def startmqtt():
-    try:
-        connectmqtt()
-        print("Starting mqtt")
-        time.sleep(5)
-    except Exception as e:
-        print("retrying..")
-        time.sleep(5)
-        print(e)
-
-
-
 while True:
-
     try:
-        startmqtt()
+        print("connectmqtt \n")
+        connectmqtt()
+        time.sleep(5)
     except Exception as e:
-        
-        time.sleep(2)
+        print("retrying startmqtt..")
+        time.sleep(5)
+        print("startmqtt exception:")
+        print(e)
