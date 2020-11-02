9334947cce992ccc1a2eb95c45e59442f0278181
tore <tore@eposten.net>
2020-11-02 14:00:53 +0000

    removed old defs

--- defs.py
+++ defs.py
@@ -1,18 +1,3 @@
-#battery_cfg="""
-#{"unit_of_measurement":"%",
-#"device_class":"battery",
-#"value_template":"{{ value_json.battery }}",
-#"state_topic":"ble2mqtt/MACADDRESS",
-#"json_attributes_topic":"ble2mqtt/MACADDRESS",
-#"name":"MACADDRESS_battery",
-#"unique_id":"MACADDRESS_battery_ble2mqtt",
-#"device":{"identifiers":["ble2mqtt_MACADDRESS"],
-#"name":"MACADDRESS",
-#"sw_version":"ble2mqtt 1.0",
-#"model":"MODELNAME",
-#"manufacturer":"MANUFACTURENAME"},
-#"availability_topic":"ble2mqtt/state"}"""
-
 battery_cfg = """
 {
   "availability_topic": "ble2mqtt/state",
@@ -33,21 +18,6 @@ battery_cfg = """
   "unit_of_measurement": "%",
   "value_template": "{{ value_json.battery }}"
 }"""
-
-#signal_cfg = """
-#{"icon":"mdi:signal",
-#"unit_of_measurement":"lqi",
-#"value_template":"{{ value_json.linkquality }}",
-#"state_topic":"ble2mqtt/MACADDRESS",
-#"json_attributes_topic":"ble2mqtt/MACADDRESS",
-#"name":"MACADDRESS_linkquality",
-#"unique_id":"MACADDRESS_linkquality_ble2mqtt",
-#"device":{"identifiers":["ble2mqtt_MACADDRESS"],
-#"name":"MACADDRESS",
-#"sw_version":"ble2mqtt 1.0",
-#"model":"MODELNAME",
-#"manufacturer":"MANUFACTURENAME"},
-#"availability_topic":"ble2mqtt/state"}"""
 
 signal_cfg = """
 {
@@ -70,21 +40,6 @@ signal_cfg = """
   "value_template": "{{ value_json.linkquality }}"
 }"""
 
-#temperature_cfg = """
-#{"unit_of_measurement":"°C",
-#"device_class":"temperature",
-#"value_template":"{{ value_json.temperature }}",
-#"state_topic":"ble2mqtt/MACADDRESS",
-#"json_attributes_topic":"ble2mqtt/MACADDRESS",
-#"name":"MACADDRESS_temperature",
-#"unique_id":"MACADDRESS_temperature_ble2mqtt",
-#"device":{"identifiers":["ble2mqtt_MACADDRESS"],
-#"name":"MACADDRESS",
-#"sw_version":"ble2mqtt 1.0",
-#"model":"MODELNAME",
-#"manufacturer":"MANUFACTURENAME"},
-#"availability_topic":"ble2mqtt/state"}"""
-
 temperature_cfg = """
 {
   "availability_topic": "ble2mqtt/state",
@@ -105,21 +60,6 @@ temperature_cfg = """
   "unit_of_measurement": "°C",
   "value_template": "{{ value_json.temperature }}"
 }"""
-
-#humidity_cfg = """
-#{"unit_of_measurement":"%",
-#"device_class":"humidity",
-#"value_template":"{{ value_json.humidity }}",
-#"state_topic":"ble2mqtt/MACADDRESS",
-#"json_attributes_topic":"ble2mqtt/MACADDRESS",
-#"name":"MACADDRESS_humidity",
-#"unique_id":"MACADDRESS_humidity_ble2mqtt",
-#"device":{"identifiers":["ble2mqtt_MACADDRESS"],
-#"name":"MACADDRESS",
-#"sw_version":"ble2mqtt 1.0",
-#"model":"MODELNAME",
-#"manufacturer":"MANUFACTURENAME"},
-#"availability_topic":"ble2mqtt/state"}"""
 
 humidity_cfg = """
 {
